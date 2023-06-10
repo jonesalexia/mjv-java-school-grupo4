d0491541973532c2d41928ea7bb831b3291a8c9a
italloramon <irvp@ic.ufal.br>
2023-06-10 16:56:37 -0300

    feat: Adicionando atributo nome do cliente e metodo cancelar

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -2,12 +2,15 @@ public class ContaCorrente {
     
     String numeroDaConta;
     String numeroDaAgencia;
+    String nomeCliente;
 
     //Sacar
     public void sacar(double valor){}
 
     //Tranferir
     public void transferir(contaDestinatario, double valor) {}
+
+    public void cancelar(String justificativa) {}
 }
 
 
